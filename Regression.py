907f2ddcc1c0ca495458b66e224c2e2692325df1
Osama <osamabinesmail@gmail.com>
2018-11-20 21:52:52 -0600

    split data into training and validation

--- Regression.py
+++ Regression.py
@@ -17,7 +17,7 @@ with open('all/train.csv', newline='') as housing_data_file:
         for i in range(1, len(house)):
             house[i] = float(house[i])
         x_train.append(house[1:-1])
-        y_train.append([house[-1]])
+        y_train.append(house[-1])
 
 # converting python lists to numpy arrays
 x_train = np.asarray(x_train)
@@ -27,13 +27,13 @@ y_train = np.asarray(y_train)
 for i in range(13):
     x_train[:,i] /= max(abs(x_train[:,i]))
 
-y_train /= max(abs(y_train[:,0]))
+y_train /= max(abs(y_train))
 
 # define our model
 model = Sequential()
 model.add(Dense(units=1, activation='sigmoid', input_dim=13))
 model.compile(optimizer='sgd', loss='mse', metrics=['accuracy'])
-model.fit(x_train, y_train, epochs=10000)
+model.fit(x_train, y_train, epochs=10000, validation_split=0.2)
 
 # 2 arrays: first one has weights (m in y = mx+b), second has biases (b in y = mx+b) 
 print(model.get_weights())
